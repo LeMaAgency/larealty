b37c947eb90246126c1fad00d483f2ee31cb1743
TmWeb <kvotvet2@vh110.timeweb.ru>
2018-03-20 14:23:33 +0300

    init

---
+++ local/templates/main/footer.php
@@ -0,0 +1,39 @@
+
+
+
+    <footer class="footer">
+        <div class="container">
+            <div class="footer-top">
+                <div class="footer-logo"><img src="img/footer-logo.png" alt="img"></div>
+                <nav class="footer-menu">
+                    <a href="#" class="footer-menu-link link-hvr">Главная</a>
+                    <a href="#" class="footer-menu-link link-hvr">Каталог квартир</a>
+                    <a href="#" class="footer-menu-link link-hvr">Загородная недвижимость</a>
+                    <a href="#" class="footer-menu-link link-hvr">Аренда</a>
+                    <a href="#" class="footer-menu-link link-hvr">Ипотека</a>
+                    <a href="#" class="footer-menu-link link-hvr">Продавцу</a>
+                    <a href="#" class="footer-menu-link link-hvr">Покупателю</a>
+                </nav>
+            </div>
+            <div class="footer-bottom">
+                <div class="footer-copyright">Квартирный ответ. Все права защищены</div>
+                <div class="footer-social">
+                    <a href="#" class="footer-social-link fb"></a>
+                    <a href="#" class="footer-social-link vk"></a>
+                    <a href="#" class="footer-social-link tw"></a>
+                    <a href="#" class="footer-social-link inst"></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/nouislider.min.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBr4vuLpC6apKgl5YdtNOgYqjiRerk8X_I&callback=initMap" async defer></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
---
+++ local/templates/main/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Загородная Объект Коттедж</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i&amp;subset=cyrillic" rel="stylesheet">
+    <link rel="stylesheet" href="css/fonts/fonts.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/media.css">
+</head>
+
+<body class="catalog">
+    <header class="header">
+        <div class="header-top-line">
+            <div class="container">
+                <nav class="header-top-menu">
+                    <a href="#" class="header-top-menu-link">Контакты</a>
+                    <a href="#" class="header-top-menu-link">Акции</a>
+                    <a href="#" class="header-top-menu-link">Вакансии</a>
+                </nav>
+            </div>
+        </div>
+        <div class="header-contacts container-index">
+            <div class="header-logo"><img src="img/logo.png"></div>
+            <div class="header-contacts-block">
+                <div class="header-phone">+7 (4242) 399-199</div>
+                <div class="header-account">
+                    <a href="#" class="header-account-link signin-link link-hvr">Вход</a>
+                    <span class="header-account-border"></span>
+                    <a href="#" class="header-account-link signup-link link-hvr">Регистрация</a>
+                </div>
+            </div>
+        </div>
+        <div class="header-main-menu">
+            <div class="container">
+                <div class="mobile-menu"></div>
+                <nav class="header-menu">
+                    <a href="#" class="header-menu-link hvr-sweep-to-top">Главная</a>
+                    <a href="#" class="header-menu-link hvr-sweep-to-top">Каталог квартир</a>
+                    <a href="#" class="header-menu-link hvr-sweep-to-top">Загородная недвижимость</a>
+                    <a href="#" class="header-menu-link hvr-sweep-to-top">Аренда</a>
+                    <a href="#" class="header-menu-link hvr-sweep-to-top">Ипотека</a>
+                    <a href="#" class="header-menu-link hvr-sweep-to-top">Продавцу</a>
+                    <a href="#" class="header-menu-link hvr-sweep-to-top">Покупателю</a>
+                </nav>
+            </div>
+        </div>
+    </header>
+
