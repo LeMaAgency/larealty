e837eb0ecc6d13cea2306b2b289e683875d3abf4
DoNotEvenThink <alekseivasin1993@gmail.com>
2018-03-23 19:42:07 +0300

    db and service_package

--- buyer/index.php
+++ buyer/index.php
@@ -53,58 +53,75 @@ $APPLICATION->SetTitle("Покупателю");
                 </div>
             </div>
         </div>
-
-        <div class="service-package">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <h2 class="service-package__h2">В пакет наших услуг входит:</h2>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-6">
-                        <div class="service-package__item">
-                            <div class="service-package__item__img service-package__item__img_1"></div>
-                            <div class="service-package__item__text">Подбор наиболее подходящих объектов согласно Вашим
-                                потребностям;
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-6">
-                        <div class="service-package__item">
-                            <div class="service-package__item__img service-package__item__img_2"></div>
-                            <div class="service-package__item__text">Показы недвижимости в удобное для Вас время;</div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-6">
-                        <div class="service-package__item">
-                            <div class="service-package__item__img service-package__item__img_3"></div>
-                            <div class="service-package__item__text">Помощь в оформлении ипотеки в ведущих банках
-                                города
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-6">
-                        <div class="service-package__item">
-                            <div class="service-package__item__img service-package__item__img_4"></div>
-                            <div class="service-package__item__text">Профессиональные рекомендации по соотношению
-                                «цена-предложение»
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-6">
-                        <div class="service-package__item">
-                            <div class="service-package__item__img service-package__item__img_5"></div>
-                            <div class="service-package__item__text">Оценка юридической чистоты объекта</div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-6">
-                        <div class="service-package__item">
-                            <div class="service-package__item__img service-package__item__img_6"></div>
-                            <div class="service-package__item__text">Гарантии сделки</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <!-- SERVICES_PACKAGE -->
+        <? $APPLICATION->IncludeComponent(
+	"bitrix:news.list", 
+	"services_package", 
+	array(
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"AJAX_MODE" => "N",
+		"IBLOCK_TYPE" => "content",
+		"IBLOCK_ID" => "5",
+		"NEWS_COUNT" => "20",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_ORDER1" => "DESC",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER2" => "ASC",
+		"FILTER_NAME" => "",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"SET_TITLE" => "N",
+		"SET_BROWSER_TITLE" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "Y",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "services_package",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_FILTER" => "Y",
+		"CACHE_GROUPS" => "N",
+		"DISPLAY_TOP_PAGER" => "Y",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Элементы",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => "",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"SET_STATUS_404" => "N",
+		"SHOW_404" => "N",
+		"MESSAGE_404" => "",
+		"PAGER_BASE_LINK" => "",
+		"PAGER_PARAMS_NAME" => "arrPager",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"COMPONENT_TEMPLATE" => "services_package",
+		"STRICT_SECTION_CHECK" => "N"
+	),
+	false
+); ?>
+        <!-- /SERVICES_PACKAGE -->
         <!-- FAVORABLY -->
         <? $APPLICATION->IncludeComponent(
             "bitrix:news.detail",
