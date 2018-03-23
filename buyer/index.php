28c9c6a88d252828d2dc9f60f2a15011333bfc8b
DoNotEvenThink <alekseivasin1993@gmail.com>
2018-03-23 18:59:40 +0300

    add reviews

--- buyer/index.php
+++ buyer/index.php
@@ -237,28 +237,29 @@ $APPLICATION->SetTitle("Покупателю");
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h3 class="connect-consultant__title">Свяжитесь с нашим представителем<br><span>чтобы получить подробную консультацию и начать поиск квартиры уже сегодня.</span>
+                        <h3 class="connect-consultant__title">
+                            <? $APPLICATION->IncludeFile(SITE_DIR . 'include/buyer/reviews/title.php'); ?>
                         </h3>
                         <? $APPLICATION->IncludeComponent(
-	"lema:form.ajax", 
-	"consultation", 
-	array(
-		"COMPONENT_TEMPLATE" => "consultation",
-		"FORM_CLASS" => "ajax-form connect-consultant__form feedback-form",
-		"FORM_ACTION" => "",
-		"FORM_152_FZ" => "Я ознакомлен <a target=\"_blank\" href=\"/contacts/apply.pdf\">c положением об обработке и защите персональных данных.</a>",
-		"FORM_BTN_TITLE" => "Получить консультацию",
-		"FORM_SUCCESS_FUNCTION" => "\$.fancybox.open(\"Ваше сообщение успешно отправлено\")",
-		"FORM_SUCCESS_FUNCTION_CORRECT_JSON" => "Y",
-		"FORM_FIELDS" => "[{\"name\":\"name\",\"type\":\"text\",\"title\":\"\",\"placeholder\":\"Имя\",\"default\":\"\",\"required\":\"Y\"},{\"name\":\"phone\",\"type\":\"tel\",\"title\":\"\",\"placeholder\":\"Телефон\",\"default\":\"\",\"required\":\"Y\"}]",
-		"NEED_SAVE_TO_IBLOCK" => "N",
-		"NEED_SEND_EMAIL" => "Y",
-		"EVENT_TYPE" => "57",
-		"CACHE_TYPE" => "A",
-		"CACHE_TIME" => "3600"
-	),
-	false
-); ?>
+                            "lema:form.ajax",
+                            "consultation",
+                            array(
+                                "COMPONENT_TEMPLATE" => "consultation",
+                                "FORM_CLASS" => "ajax-form connect-consultant__form feedback-form",
+                                "FORM_ACTION" => "",
+                                "FORM_152_FZ" => "Я ознакомлен <a target=\"_blank\" href=\"/contacts/apply.pdf\">c положением об обработке и защите персональных данных.</a>",
+                                "FORM_BTN_TITLE" => "Получить консультацию",
+                                "FORM_SUCCESS_FUNCTION" => "\$.fancybox.open(\"Ваше сообщение успешно отправлено\")",
+                                "FORM_SUCCESS_FUNCTION_CORRECT_JSON" => "Y",
+                                "FORM_FIELDS" => "[{\"name\":\"name\",\"type\":\"text\",\"title\":\"\",\"placeholder\":\"Имя\",\"default\":\"\",\"required\":\"Y\"},{\"name\":\"phone\",\"type\":\"tel\",\"title\":\"\",\"placeholder\":\"Телефон\",\"default\":\"\",\"required\":\"Y\"}]",
+                                "NEED_SAVE_TO_IBLOCK" => "N",
+                                "NEED_SEND_EMAIL" => "Y",
+                                "EVENT_TYPE" => "57",
+                                "CACHE_TYPE" => "A",
+                                "CACHE_TIME" => "3600"
+                            ),
+                            false
+                        ); ?>
                     </div>
                 </div>
             </div>
@@ -371,44 +372,76 @@ $APPLICATION->SetTitle("Покупателю");
             </div>
         </section>
 
-        <div class="customer-reviews">
-            <div class="container">
-                <h2 class="customer-reviews__h2">«Отзывы наших клиентов»</h2>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="customer-reviews__item">
-                            <div class="customer-reviews__item__icon"></div>
-                            <div class="customer-reviews__item__content">
-                                <div class="customer-reviews__item__content__name">Дмитрий Иванов</div>
-                                <span class="customer-reviews__item__content__time">27.09.2017</span>
-                                <p class="customer-reviews__item__content__text">Свяжитесь с нашим представителем, чтобы
-                                    получить бесплатную консультацию о продаже и пригласить риелтора на осмотр и
-                                    оценку</p>
-                                <a href="#" class="customer-reviews__item__content__detail"><span>Подробнее</span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="customer-reviews__item">
-                            <div class="customer-reviews__item__icon"></div>
-                            <div class="customer-reviews__item__content">
-                                <div class="customer-reviews__item__content__name">Дмитрий Иванов</div>
-                                <span class="customer-reviews__item__content__time">27.09.2017</span>
-                                <p class="customer-reviews__item__content__text">Свяжитесь с нашим представителем, чтобы
-                                    получить бесплатную консультацию о продаже и пригласить риелтора на осмотр и
-                                    оценку</p>
-                                <a href="#" class="customer-reviews__item__content__detail"><span>Подробнее</span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <a class="customer-reviews__more icon-right-small" href="#">
-                    <span>Показать еще</span>
-                </a>
-            </div>
-        </div>
+        <!-- REVIEWS -->
+        <? $APPLICATION->IncludeComponent(
+            "bitrix:news.list",
+            "reviews",
+            array(
+                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                "ADD_SECTIONS_CHAIN" => "N",
+                "AJAX_MODE" => "N",
+                "AJAX_OPTION_ADDITIONAL" => "",
+                "AJAX_OPTION_HISTORY" => "N",
+                "AJAX_OPTION_JUMP" => "N",
+                "AJAX_OPTION_STYLE" => "Y",
+                "CACHE_FILTER" => "N",
+                "CACHE_GROUPS" => "Y",
+                "CACHE_TIME" => "36000000",
+                "CACHE_TYPE" => "A",
+                "CHECK_DATES" => "Y",
+                "DETAIL_URL" => "",
+                "DISPLAY_BOTTOM_PAGER" => "Y",
+                "DISPLAY_DATE" => "N",
+                "DISPLAY_NAME" => "N",
+                "DISPLAY_PICTURE" => "N",
+                "DISPLAY_PREVIEW_TEXT" => "N",
+                "DISPLAY_TOP_PAGER" => "N",
+                "FIELD_CODE" => array(
+                    0 => "NAME",
+                    1 => "PREVIEW_TEXT",
+                    2 => "PREVIEW_PICTURE",
+                    3 => "",
+                ),
+                "FILTER_NAME" => "",
+                "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                "IBLOCK_ID" => "8",
+                "IBLOCK_TYPE" => "content",
+                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                "INCLUDE_SUBSECTIONS" => "N",
+                "MESSAGE_404" => "",
+                "NEWS_COUNT" => "2",
+                "PAGER_BASE_LINK_ENABLE" => "N",
+                "PAGER_DESC_NUMBERING" => "N",
+                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                "PAGER_SHOW_ALL" => "N",
+                "PAGER_SHOW_ALWAYS" => "N",
+                "PAGER_TEMPLATE" => ".default",
+                "PAGER_TITLE" => "Новости",
+                "PARENT_SECTION" => "",
+                "PARENT_SECTION_CODE" => "",
+                "PREVIEW_TRUNCATE_LEN" => "",
+                "PROPERTY_CODE" => array(
+                    0 => "",
+                    1 => "",
+                ),
+                "SET_BROWSER_TITLE" => "N",
+                "SET_LAST_MODIFIED" => "N",
+                "SET_META_DESCRIPTION" => "N",
+                "SET_META_KEYWORDS" => "N",
+                "SET_STATUS_404" => "N",
+                "SET_TITLE" => "N",
+                "SHOW_404" => "N",
+                "SORT_BY1" => "SORT",
+                "SORT_BY2" => "ACTIVE_FROM",
+                "SORT_ORDER1" => "ASC",
+                "SORT_ORDER2" => "ASC",
+                "STRICT_SECTION_CHECK" => "N",
+                "COMPONENT_TEMPLATE" => "reviews"
+            ),
+            false
+        ); ?>
+        <!-- /REVIEWS -->
 
-        <!-- end-  content-page_color      -->
     </div>
 
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
