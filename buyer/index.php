88d01a2a40c145c266c3f778233e6a3b4c6db30c
DoNotEvenThink <alekseivasin1993@gmail.com>
2018-03-23 16:35:21 +0300

    add feedback form consultation and news.detail block_text and block_list

--- buyer/index.php
+++ buyer/index.php
@@ -105,63 +105,160 @@ $APPLICATION->SetTitle("Покупателю");
                 </div>
             </div>
         </div>
-        <div class="favorably">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-9 col-lg-7">
-                        <h2 class="favorably__h2">Почему выгоднее покупать недвижимость через агентство «Квартирный
-                            ответ»?</h2>
-                    </div>
-                    <div class="col-md-6">
-                        <ol class="favorably__list">
-                            <li class="favorably__list__item favorably__list__item_1">Агенты «Квартирного ответа» имеют
-                                доступ к актуальным предложениям, которые обновляются в онлайн-режиме.
-                            </li>
-                            <li class="favorably__list__item favorably__list__item_2">Агентство на 100% защищает
-                                покупателя от финансовых и юридических рисков.
-                            </li>
-                            <li class="favorably__list__item favorably__list__item_3">Риелтор полностью берет на себя
-                                вопросы торга.
-                            </li>
-                            <li class="favorably__list__item favorably__list__item_4">Стоимость недвижимости через
-                                агентство может быть выгоднее даже с учетом комиссии.
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="we-check-history">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8">
-                        <h2 class="we-check-history__title">У каждой квартиры своя история, и мы готовы ее
-                            проверить!</h2>
-                        <p class="we-check-history__text">По статистике, в сделке купли-продажи недвижимости всегда
-                            больше рискует покупатель. Обратившись к экспертам компании «Квартирный ответ», вы сможете
-                            получить доступ к лучшим объектам по лучшим ценам. Мы ставим интересы клиентов на первое
-                            место, а вы получаете очевидный результат<b> – юридически чистую квартиру по выгодной
-                                стоимости без малейших рисков.</b></p>
-                        <p class="we-check-history__text"><b>Самая большая сделка в Вашей жизни заслуживает того, чтобы
-                                обратиться к профессионалам!</b></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <!-- FAVORABLY -->
+        <? $APPLICATION->IncludeComponent(
+            "bitrix:news.detail",
+            "blocks_list",
+            array(
+                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                "ADD_ELEMENT_CHAIN" => "N",
+                "ADD_SECTIONS_CHAIN" => "N",
+                "AJAX_MODE" => "N",
+                "AJAX_OPTION_ADDITIONAL" => "",
+                "AJAX_OPTION_HISTORY" => "N",
+                "AJAX_OPTION_JUMP" => "N",
+                "AJAX_OPTION_STYLE" => "Y",
+                "BROWSER_TITLE" => "-",
+                "CACHE_GROUPS" => "Y",
+                "CACHE_TIME" => "36000000",
+                "CACHE_TYPE" => "A",
+                "CHECK_DATES" => "Y",
+                "DETAIL_URL" => "",
+                "DISPLAY_BOTTOM_PAGER" => "Y",
+                "DISPLAY_DATE" => "N",
+                "DISPLAY_NAME" => "N",
+                "DISPLAY_PICTURE" => "N",
+                "DISPLAY_PREVIEW_TEXT" => "N",
+                "DISPLAY_TOP_PAGER" => "N",
+                "ELEMENT_CODE" => "favorably",
+                "ELEMENT_ID" => "",
+                "FIELD_CODE" => array(
+                    0 => "CODE",
+                    1 => "PREVIEW_TEXT",
+                    2 => "DETAIL_TEXT",
+                    3 => "",
+                ),
+                "IBLOCK_ID" => "7",
+                "IBLOCK_TYPE" => "content",
+                "IBLOCK_URL" => "",
+                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                "MESSAGE_404" => "",
+                "META_DESCRIPTION" => "-",
+                "META_KEYWORDS" => "-",
+                "PAGER_BASE_LINK_ENABLE" => "N",
+                "PAGER_SHOW_ALL" => "N",
+                "PAGER_TEMPLATE" => ".default",
+                "PAGER_TITLE" => "Страница",
+                "PROPERTY_CODE" => array(
+                    0 => "LIST_ELEMENTS",
+                    1 => "",
+                ),
+                "SET_BROWSER_TITLE" => "N",
+                "SET_CANONICAL_URL" => "N",
+                "SET_LAST_MODIFIED" => "N",
+                "SET_META_DESCRIPTION" => "N",
+                "SET_META_KEYWORDS" => "N",
+                "SET_STATUS_404" => "N",
+                "SET_TITLE" => "N",
+                "SHOW_404" => "N",
+                "STRICT_SECTION_CHECK" => "N",
+                "USE_PERMISSIONS" => "N",
+                "USE_SHARE" => "N",
+                "COMPONENT_TEMPLATE" => "blocks_list"
+            ),
+            false
+        ); ?>
+        <!-- /FAVORABLY -->
+        <!-- WE CHECK HISTORY -->
+        <? $APPLICATION->IncludeComponent(
+            "bitrix:news.detail",
+            "blocks_text",
+            array(
+                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                "ADD_ELEMENT_CHAIN" => "N",
+                "ADD_SECTIONS_CHAIN" => "N",
+                "AJAX_MODE" => "N",
+                "AJAX_OPTION_ADDITIONAL" => "",
+                "AJAX_OPTION_HISTORY" => "N",
+                "AJAX_OPTION_JUMP" => "N",
+                "AJAX_OPTION_STYLE" => "Y",
+                "BROWSER_TITLE" => "-",
+                "CACHE_GROUPS" => "Y",
+                "CACHE_TIME" => "36000000",
+                "CACHE_TYPE" => "A",
+                "CHECK_DATES" => "Y",
+                "DETAIL_URL" => "",
+                "DISPLAY_BOTTOM_PAGER" => "Y",
+                "DISPLAY_DATE" => "N",
+                "DISPLAY_NAME" => "N",
+                "DISPLAY_PICTURE" => "N",
+                "DISPLAY_PREVIEW_TEXT" => "N",
+                "DISPLAY_TOP_PAGER" => "N",
+                "ELEMENT_CODE" => "we-check-history",
+                "ELEMENT_ID" => "",
+                "FIELD_CODE" => array(
+                    0 => "CODE",
+                    1 => "PREVIEW_TEXT",
+                    2 => "DETAIL_TEXT",
+                    3 => "",
+                ),
+                "IBLOCK_ID" => "7",
+                "IBLOCK_TYPE" => "content",
+                "IBLOCK_URL" => "",
+                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                "MESSAGE_404" => "",
+                "META_DESCRIPTION" => "-",
+                "META_KEYWORDS" => "-",
+                "PAGER_BASE_LINK_ENABLE" => "N",
+                "PAGER_SHOW_ALL" => "N",
+                "PAGER_TEMPLATE" => ".default",
+                "PAGER_TITLE" => "Страница",
+                "PROPERTY_CODE" => array(
+                    0 => "LIST_ELEMENTS",
+                    1 => "",
+                ),
+                "SET_BROWSER_TITLE" => "N",
+                "SET_CANONICAL_URL" => "N",
+                "SET_LAST_MODIFIED" => "N",
+                "SET_META_DESCRIPTION" => "N",
+                "SET_META_KEYWORDS" => "N",
+                "SET_STATUS_404" => "N",
+                "SET_TITLE" => "N",
+                "SHOW_404" => "N",
+                "STRICT_SECTION_CHECK" => "N",
+                "USE_PERMISSIONS" => "N",
+                "USE_SHARE" => "N",
+                "COMPONENT_TEMPLATE" => "blocks_text"
+            ),
+            false
+        ); ?>
+        <!-- /WE CHECK HISTORY -->
         <div class="connect-consultant">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
                         <h3 class="connect-consultant__title">Свяжитесь с нашим представителем<br><span>чтобы получить подробную консультацию и начать поиск квартиры уже сегодня.</span>
                         </h3>
-                        <form action="#" class="connect-consultant__form">
-                            <input type="text" class="connect-consultant__form__input" name="name"
-                                   placeholder="Ваше имя ">
-                            <input type="tel" class="connect-consultant__form__input" name="tel"
-                                   placeholder="Ваш телефон">
-                            <button class="connect-consultant__form__button" type="submit">Получить консультацию
-                            </button>
-                        </form>
+                        <? $APPLICATION->IncludeComponent(
+	"lema:form.ajax", 
+	"consultation", 
+	array(
+		"COMPONENT_TEMPLATE" => "consultation",
+		"FORM_CLASS" => "ajax-form connect-consultant__form feedback-form",
+		"FORM_ACTION" => "",
+		"FORM_152_FZ" => "Я ознакомлен <a target=\"_blank\" href=\"/contacts/apply.pdf\">c положением об обработке и защите персональных данных.</a>",
+		"FORM_BTN_TITLE" => "Получить консультацию",
+		"FORM_SUCCESS_FUNCTION" => "\$.fancybox.open(\"Ваше сообщение успешно отправлено\")",
+		"FORM_SUCCESS_FUNCTION_CORRECT_JSON" => "Y",
+		"FORM_FIELDS" => "[{\"name\":\"name\",\"type\":\"text\",\"title\":\"\",\"placeholder\":\"Имя\",\"default\":\"\",\"required\":\"Y\"},{\"name\":\"phone\",\"type\":\"tel\",\"title\":\"\",\"placeholder\":\"Телефон\",\"default\":\"\",\"required\":\"Y\"}]",
+		"NEED_SAVE_TO_IBLOCK" => "N",
+		"NEED_SEND_EMAIL" => "Y",
+		"EVENT_TYPE" => "57",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "3600"
+	),
+	false
+); ?>
                     </div>
                 </div>
             </div>
