fd9814e181a6ff9d2f156d86601a2c855c8d6a53
DoNotEvenThink <alekseivasin1993@gmail.com>
2018-03-26 19:28:48 +0300

    banners (seller and buyer) and db

--- for_buyer/index.php
+++ for_buyer/index.php
@@ -5,6 +5,7 @@ $APPLICATION->SetTitle("Покупателю");
 
     <div class="content-page_color">
 
+        <!-- BANNER_WHY_NOT_FOR_BUYER -->
         <div class="why-not">
             <div class="container">
                 <div class="row">
@@ -15,44 +16,78 @@ $APPLICATION->SetTitle("Покупателю");
                         <p class="why-not__title">
                             <? $APPLICATION->IncludeFile(SITE_DIR . 'include/main/why-not/subtitle.php'); ?>
                         </p>
-                        <div class="row">
-                            <div class="col-md-5 col-sm-6">
-                                <h4 class="why-not__h4 why-not__h4_1">
-                                    <? $APPLICATION->IncludeFile(SITE_DIR . 'include/main/why-not/block-1/title.php'); ?>
-                                </h4>
-                                <p class="why-not__text">
-                                    <? $APPLICATION->IncludeFile(SITE_DIR . 'include/main/why-not/block-1/description.php'); ?>
-                                </p>
-                            </div>
-                            <div class="col-md-5 col-sm-6">
-                                <h4 class="why-not__h4 why-not__h4_2">
-                                    <? $APPLICATION->IncludeFile(SITE_DIR . 'include/main/why-not/block-2/title.php'); ?>
-                                </h4>
-                                <p class="why-not__text">
-                                    <? $APPLICATION->IncludeFile(SITE_DIR . 'include/main/why-not/block-2/description.php'); ?>
-                                </p>
-                            </div>
-                            <div class="col-md-5 col-sm-6">
-                                <h4 class="why-not__h4 why-not__h4_3">
-                                    <? $APPLICATION->IncludeFile(SITE_DIR . 'include/main/why-not/block-3/title.php'); ?>
-                                </h4>
-                                <p class="why-not__text">
-                                    <? $APPLICATION->IncludeFile(SITE_DIR . 'include/main/why-not/block-3/description.php'); ?>
-                                </p>
-                            </div>
-                            <div class="col-md-5 col-sm-6">
-                                <h4 class="why-not__h4 why-not__h4_4">
-                                    <? $APPLICATION->IncludeFile(SITE_DIR . 'include/main/why-not/block-4/title.php'); ?>
-                                </h4>
-                                <p class="why-not__text">
-                                    <? $APPLICATION->IncludeFile(SITE_DIR . 'include/main/why-not/block-4/description.php'); ?>
-                                </p>
-                            </div>
-                        </div>
+                        <? $APPLICATION->IncludeComponent(
+                            "bitrix:news.list",
+                            "why_not_for_buyer",
+                            array(
+                                "DISPLAY_DATE" => "Y",
+                                "DISPLAY_NAME" => "Y",
+                                "DISPLAY_PICTURE" => "Y",
+                                "DISPLAY_PREVIEW_TEXT" => "Y",
+                                "AJAX_MODE" => "N",
+                                "IBLOCK_TYPE" => "content",
+                                "IBLOCK_ID" => "7",
+                                "NEWS_COUNT" => "20",
+                                "SORT_BY1" => "ACTIVE_FROM",
+                                "SORT_ORDER1" => "DESC",
+                                "SORT_BY2" => "SORT",
+                                "SORT_ORDER2" => "ASC",
+                                "FILTER_NAME" => "",
+                                "FIELD_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "PROPERTY_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "CHECK_DATES" => "Y",
+                                "DETAIL_URL" => "",
+                                "PREVIEW_TRUNCATE_LEN" => "",
+                                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                                "SET_TITLE" => "N",
+                                "SET_BROWSER_TITLE" => "N",
+                                "SET_META_KEYWORDS" => "N",
+                                "SET_META_DESCRIPTION" => "N",
+                                "SET_LAST_MODIFIED" => "N",
+                                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                                "ADD_SECTIONS_CHAIN" => "N",
+                                "HIDE_LINK_WHEN_NO_DETAIL" => "Y",
+                                "PARENT_SECTION" => "",
+                                "PARENT_SECTION_CODE" => "banner_why_not_for_buyer",
+                                "INCLUDE_SUBSECTIONS" => "Y",
+                                "CACHE_TYPE" => "A",
+                                "CACHE_TIME" => "36000000",
+                                "CACHE_FILTER" => "Y",
+                                "CACHE_GROUPS" => "N",
+                                "DISPLAY_TOP_PAGER" => "Y",
+                                "DISPLAY_BOTTOM_PAGER" => "Y",
+                                "PAGER_TITLE" => "Элементы",
+                                "PAGER_SHOW_ALWAYS" => "N",
+                                "PAGER_TEMPLATE" => "",
+                                "PAGER_DESC_NUMBERING" => "N",
+                                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                                "PAGER_SHOW_ALL" => "N",
+                                "PAGER_BASE_LINK_ENABLE" => "N",
+                                "SET_STATUS_404" => "N",
+                                "SHOW_404" => "N",
+                                "MESSAGE_404" => "",
+                                "PAGER_BASE_LINK" => "",
+                                "PAGER_PARAMS_NAME" => "arrPager",
+                                "AJAX_OPTION_JUMP" => "N",
+                                "AJAX_OPTION_STYLE" => "Y",
+                                "AJAX_OPTION_HISTORY" => "N",
+                                "AJAX_OPTION_ADDITIONAL" => "",
+                                "COMPONENT_TEMPLATE" => "why_not_for_buyer",
+                                "STRICT_SECTION_CHECK" => "N"
+                            ),
+                            false
+                        ); ?>
                     </div>
                 </div>
             </div>
         </div>
+        <!-- /BANNER_WHY_NOT_FOR_BUYER -->
         <!-- SERVICES_PACKAGE -->
         <? $APPLICATION->IncludeComponent(
             "bitrix:news.list",
--- for_seller/index.php
+++ for_seller/index.php
@@ -2,68 +2,153 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Продавцу");
 ?>
+    <div class="content-page_color">
+
+        <div class="seller_main">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h2 class="seller_main__h2">
+                            <? $APPLICATION->IncludeFile(SITE_DIR . 'include/seller/banner/title.php'); ?>
+                        </h2>
+
+                        <p class="seller_main__title">
+                            <? $APPLICATION->IncludeFile(SITE_DIR . 'include/seller/banner/subtitle.php'); ?>
+                        </p>
+                        <? $APPLICATION->IncludeComponent(
+                            "bitrix:news.list",
+                            "banner_for_seller",
+                            array(
+                                "DISPLAY_DATE" => "Y",
+                                "DISPLAY_NAME" => "Y",
+                                "DISPLAY_PICTURE" => "Y",
+                                "DISPLAY_PREVIEW_TEXT" => "Y",
+                                "AJAX_MODE" => "N",
+                                "IBLOCK_TYPE" => "content",
+                                "IBLOCK_ID" => "7",
+                                "NEWS_COUNT" => "20",
+                                "SORT_BY1" => "ACTIVE_FROM",
+                                "SORT_ORDER1" => "DESC",
+                                "SORT_BY2" => "SORT",
+                                "SORT_ORDER2" => "ASC",
+                                "FILTER_NAME" => "",
+                                "FIELD_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "PROPERTY_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "CHECK_DATES" => "Y",
+                                "DETAIL_URL" => "",
+                                "PREVIEW_TRUNCATE_LEN" => "",
+                                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                                "SET_TITLE" => "N",
+                                "SET_BROWSER_TITLE" => "N",
+                                "SET_META_KEYWORDS" => "N",
+                                "SET_META_DESCRIPTION" => "N",
+                                "SET_LAST_MODIFIED" => "N",
+                                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                                "ADD_SECTIONS_CHAIN" => "N",
+                                "HIDE_LINK_WHEN_NO_DETAIL" => "Y",
+                                "PARENT_SECTION" => "",
+                                "PARENT_SECTION_CODE" => "banner_seller_for_seller",
+                                "INCLUDE_SUBSECTIONS" => "Y",
+                                "CACHE_TYPE" => "A",
+                                "CACHE_TIME" => "36000000",
+                                "CACHE_FILTER" => "Y",
+                                "CACHE_GROUPS" => "N",
+                                "DISPLAY_TOP_PAGER" => "Y",
+                                "DISPLAY_BOTTOM_PAGER" => "Y",
+                                "PAGER_TITLE" => "Элементы",
+                                "PAGER_SHOW_ALWAYS" => "N",
+                                "PAGER_TEMPLATE" => "",
+                                "PAGER_DESC_NUMBERING" => "N",
+                                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                                "PAGER_SHOW_ALL" => "N",
+                                "PAGER_BASE_LINK_ENABLE" => "N",
+                                "SET_STATUS_404" => "N",
+                                "SHOW_404" => "N",
+                                "MESSAGE_404" => "",
+                                "PAGER_BASE_LINK" => "",
+                                "PAGER_PARAMS_NAME" => "arrPager",
+                                "AJAX_OPTION_JUMP" => "N",
+                                "AJAX_OPTION_STYLE" => "Y",
+                                "AJAX_OPTION_HISTORY" => "N",
+                                "AJAX_OPTION_ADDITIONAL" => "",
+                                "COMPONENT_TEMPLATE" => "banner_for_seller",
+                                "STRICT_SECTION_CHECK" => "N"
+                            ),
+                            false
+                        ); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- REASON -->
 <? $APPLICATION->IncludeComponent(
-	"bitrix:news.detail", 
-	"blocks_list", 
-	array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_ELEMENT_CHAIN" => "N",
-		"ADD_SECTIONS_CHAIN" => "N",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"BROWSER_TITLE" => "-",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"DISPLAY_DATE" => "N",
-		"DISPLAY_NAME" => "N",
-		"DISPLAY_PICTURE" => "N",
-		"DISPLAY_PREVIEW_TEXT" => "N",
-		"DISPLAY_TOP_PAGER" => "N",
-		"ELEMENT_CODE" => "reason",
-		"ELEMENT_ID" => "",
-		"FIELD_CODE" => array(
-			0 => "CODE",
-			1 => "PREVIEW_TEXT",
-			2 => "DETAIL_TEXT",
-			3 => "",
-		),
-		"IBLOCK_ID" => "7",
-		"IBLOCK_TYPE" => "content",
-		"IBLOCK_URL" => "",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-		"MESSAGE_404" => "",
-		"META_DESCRIPTION" => "-",
-		"META_KEYWORDS" => "-",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Страница",
-		"PROPERTY_CODE" => array(
-			0 => "LIST_ELEMENTS",
-			1 => "",
-		),
-		"SET_BROWSER_TITLE" => "N",
-		"SET_CANONICAL_URL" => "N",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_META_DESCRIPTION" => "N",
-		"SET_META_KEYWORDS" => "N",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "N",
-		"SHOW_404" => "N",
-		"STRICT_SECTION_CHECK" => "N",
-		"USE_PERMISSIONS" => "N",
-		"USE_SHARE" => "N",
-		"COMPONENT_TEMPLATE" => "blocks_list"
-	),
-	false
+    "bitrix:news.detail",
+    "blocks_list",
+    array(
+        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "ADD_ELEMENT_CHAIN" => "N",
+        "ADD_SECTIONS_CHAIN" => "N",
+        "AJAX_MODE" => "N",
+        "AJAX_OPTION_ADDITIONAL" => "",
+        "AJAX_OPTION_HISTORY" => "N",
+        "AJAX_OPTION_JUMP" => "N",
+        "AJAX_OPTION_STYLE" => "Y",
+        "BROWSER_TITLE" => "-",
+        "CACHE_GROUPS" => "Y",
+        "CACHE_TIME" => "36000000",
+        "CACHE_TYPE" => "A",
+        "CHECK_DATES" => "Y",
+        "DETAIL_URL" => "",
+        "DISPLAY_BOTTOM_PAGER" => "Y",
+        "DISPLAY_DATE" => "N",
+        "DISPLAY_NAME" => "N",
+        "DISPLAY_PICTURE" => "N",
+        "DISPLAY_PREVIEW_TEXT" => "N",
+        "DISPLAY_TOP_PAGER" => "N",
+        "ELEMENT_CODE" => "reason",
+        "ELEMENT_ID" => "",
+        "FIELD_CODE" => array(
+            0 => "CODE",
+            1 => "PREVIEW_TEXT",
+            2 => "DETAIL_TEXT",
+            3 => "",
+        ),
+        "IBLOCK_ID" => "7",
+        "IBLOCK_TYPE" => "content",
+        "IBLOCK_URL" => "",
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+        "MESSAGE_404" => "",
+        "META_DESCRIPTION" => "-",
+        "META_KEYWORDS" => "-",
+        "PAGER_BASE_LINK_ENABLE" => "N",
+        "PAGER_SHOW_ALL" => "N",
+        "PAGER_TEMPLATE" => ".default",
+        "PAGER_TITLE" => "Страница",
+        "PROPERTY_CODE" => array(
+            0 => "LIST_ELEMENTS",
+            1 => "",
+        ),
+        "SET_BROWSER_TITLE" => "N",
+        "SET_CANONICAL_URL" => "N",
+        "SET_LAST_MODIFIED" => "N",
+        "SET_META_DESCRIPTION" => "N",
+        "SET_META_KEYWORDS" => "N",
+        "SET_STATUS_404" => "N",
+        "SET_TITLE" => "N",
+        "SHOW_404" => "N",
+        "STRICT_SECTION_CHECK" => "N",
+        "USE_PERMISSIONS" => "N",
+        "USE_SHARE" => "N",
+        "COMPONENT_TEMPLATE" => "blocks_list"
+    ),
+    false
 ); ?>
     <!-- /REASON -->
     <div class="request">
