cd5b7b51b1b2c29672c82c2bef5044b18e3c20bd
DoNotEvenThink <alekseivasin1993@gmail.com>
2018-04-19 18:10:02 +0300

    add agent

--- local/modules/lema.lib/lib/Handlers/IblockElement.php
+++ local/modules/lema.lib/lib/Handlers/IblockElement.php
@@ -3,6 +3,7 @@
 namespace Lema\Handlers;
 
 use \Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Type\DateTime;
 use Lema\Common\Dumper;
 
 Loc::loadMessages(__FILE__);
@@ -19,6 +20,7 @@ class IblockElement
     public static function beforeAdd(array &$fields)
     {
         static::setElementRights($fields);
+        static::setReminders($fields);
     }
 
     /**
@@ -28,6 +30,7 @@ class IblockElement
     {
         static::generateElementNameAndCode($fields);
         static::setElementRights($fields);
+        static::setReminders($fields);
     }
 
     /**
@@ -115,6 +118,71 @@ class IblockElement
     }
 
     /**
+     * @param array $fields
+     *
+     * @return bool
+     */
+    protected static function setReminders(array $fields)
+    {
+        if(empty($fields['ID']))
+            return false;
+
+        if(isset($fields['IBLOCK_ID']) && $fields['IBLOCK_ID'] === \LIblock::getId('objects') && !empty($fields['PROPERTY_VALUES']))
+        {
+            //get realty property id
+            $remindDateId = \LIblock::getPropId('objects', 'REMINDER_DATE');
+            //get realty property value
+            if(empty($fields['PROPERTY_VALUES'][$remindDateId]))
+                $remindDate = 0;
+            else
+            {
+                $tmp = current($fields['PROPERTY_VALUES'][$remindDateId]);
+                $remindDate = empty($tmp) || empty($tmp['VALUE']) ? 0 : $tmp['VALUE'];
+            }
+            $agentName = '\\' . get_class() . '::remind(' . $fields['ID'] . ', ';
+
+
+            if($remindDate == date('d.m.Y'))
+            {
+                $remindTimeStamp = strtotime('+1 hour');
+                $remindDate = date('d.m.Y', $remindTimeStamp);
+            }
+            else
+            {
+                $remindTimeStamp = strtotime($remindDate);
+            }
+
+            //search existing agent
+            $res = \CAgent::GetList(array('ID' => 'DESC'), array('NAME' => $agentName . '%'));
+            if($row = $res->Fetch())
+            {
+                \CAgent::Update($row['ID'], array(
+                    $agentName . '"' . $remindDate . '");',
+                    '',
+                    'N',
+                    86400,
+                    date('d.m.Y H:i:s', $remindTimeStamp),
+                    'Y',
+                    date('d.m.Y H:i:s', $remindTimeStamp),
+                    30
+                ));
+            }
+
+            //Agent is not exists, create it now
+            \CAgent::AddAgent(
+                $agentName . '"' . $remindDate . '");',
+                '',
+                'N',
+                86400,
+                date('d.m.Y H:i:s', $remindTimeStamp),
+                'Y',
+                date('d.m.Y H:i:s', $remindTimeStamp),
+                30
+            );
+        }
+    }
+
+    /**
      * Set element rights for specified user (or clean it)
      *
      * @param array $fields
@@ -183,5 +251,14 @@ class IblockElement
             }
 
         }
+    }
+
+    public static function remind($objectId, $date)
+    {
+        ?>
+        <script>
+            alert('OBJECT REMIND, ID: <?=$objectId;?>, DATE: <?=$date;?>');
+        </script>
+        <?exit;
     }
 }
