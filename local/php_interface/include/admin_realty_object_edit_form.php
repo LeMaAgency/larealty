fc33022e1af443844bee1abee8a2f7ad2d949af2
DoNotEvenThink <alekseivasin1993@gmail.com>
2018-06-05 13:07:04 +0300

    prop sort

--- local/php_interface/include/admin_realty_object_edit_form.php
+++ local/php_interface/include/admin_realty_object_edit_form.php
@@ -1854,6 +1854,7 @@ if (
              */
             $('[id^="tr_PROPERTY_"], #tr_NAME, #tr_CODE').hide();
             for(var propId in initShowedProps) {
+                console.log('#tr_PROPERTY_' + initShowedProps[propId]);
                 $('#tr_PROPERTY_' + initShowedProps[propId]).show();
             }
 
