fa33ca704b3acbd738c34a952970087db99b2f79
DoNotEvenThink <alekseivasin1993@gmail.com>
2018-06-20 16:00:34 +0300

    add feature block for page rent (Arenda)

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,21 +1,29 @@
 <?
 $arUrlRewrite = array(
 	array(
-		'CONDITION' => '#^/vakancies/#',
-		'RULE' => '',
-		'ID' => 'bitrix:news',
-		'PATH' => '/vakancies/index.php',
+		"CONDITION" => "#^/vakancies/#",
+		"RULE" => "",
+		"ID" => "bitrix:news",
+		"PATH" => "/vakancies/index.php",
 	),
 	array(
-		'CONDITION' => '#^/stocks/#',
-		'RULE' => '',
-		'ID' => 'bitrix:news',
-		'PATH' => '/stocks/index.php',
+		"CONDITION" => "#^/catalog/#",
+		"RULE" => "",
+		"ID" => "bitrix:news",
+		"PATH" => "/catalog/index.php",
 	),
-    array(
-        'CONDITION' => '#^/catalog/#',
-        'RULE' => '',
-        'ID' => 'bitrix:news',
-        'PATH' => '/catalog/index.php',
-    ),
+	array(
+		"CONDITION" => "#^/stocks/#",
+		"RULE" => "",
+		"ID" => "bitrix:news",
+		"PATH" => "/stocks/index.php",
+	),
+	array(
+		"CONDITION" => "#^/rent/#",
+		"RULE" => "",
+		"ID" => "lema:news",
+		"PATH" => "/rent/index.php",
+	),
 );
+
+?>
