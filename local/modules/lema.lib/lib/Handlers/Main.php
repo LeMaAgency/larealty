b8d2628c93e8f2e9f816e0473f4d0af76c4e1912
KOPOJI <kopojiq@gmail.com>
2018-06-22 16:44:37 +0300

    change admin form for inner sections

--- local/modules/lema.lib/lib/Handlers/Main.php
+++ local/modules/lema.lib/lib/Handlers/Main.php
@@ -13,7 +13,8 @@ Loc::loadMessages(__FILE__);
  */
 class Main
 {
-    const ADMIN_LIST_FILENAME = '/bitrix/admin/la_iblock_list_admin.php';
+    const ADMIN_NEW_LIST_FILENAME = '/bitrix/admin/la_iblock_list_admin.php';
+    const ADMIN_DEFAULT_LIST_FILENAME = '/bitrix/admin/iblock_list_admin.php';
     /**
      * @param $list
      */
@@ -27,12 +28,13 @@ class Main
                 'GLOBAL_ICON' => 'adm-menu-copy',
             );
         }
+
         /**
          * Auto-Redirect to our new page
          */
-        if(isset($_GET['IBLOCK_ID']) && $_GET['IBLOCK_ID'] == 2 && Request::get()->getPhpSelf() != static::ADMIN_LIST_FILENAME)
+        if(isset($_GET['IBLOCK_ID']) && $_GET['IBLOCK_ID'] == 2 && Request::get()->getPhpSelf() == static::ADMIN_DEFAULT_LIST_FILENAME)
         {
-            LocalRedirect(static::ADMIN_LIST_FILENAME . '?' . http_build_query($_GET));
+            LocalRedirect(static::ADMIN_NEW_LIST_FILENAME . '?' . http_build_query($_GET));
             exit;
         }
     }
