a8b579928b49d927576e3cfd0001ee2620e79a69
KOPOJI <kopojiq@gmail.com>
2018-06-27 12:01:35 +0300

    fix autofit

--- local/modules/lema.lib/lib/Subscribe/AutoFit.php
+++ local/modules/lema.lib/lib/Subscribe/AutoFit.php
@@ -62,22 +62,12 @@ class AutoFit
          * Get start timestamp
          */
         $startDateTime = \DateTime::createFromFormat('d.m.Y H:i', $dateFrom . ' ' . $timeStart);
+        if($startDateTime->getTimestamp() < (new \DateTime('now'))->getTimestamp())
+            $startDateTime->modify('+1 day');
         /**
          * Get end timestamp
          */
         $endDateTime = \DateTime::createFromFormat('d.m.Y H:i', $dateTo . ' ' . $timeStart);
-        $now = (new \DateTime('now'))->getTimestamp();
-        /**
-         * Remove old agents and exit
-         */
-        if($startDateTime->getTimestamp() > $now || $endDateTime->getTimestamp() > $now)
-        {
-            static::removeAgent($requestId);
-            return false;
-        }
-
-        if($startDateTime->getTimestamp() < $now)
-            $startDateTime->modify('+1 day');
 
         $agentName = sprintf(
             '\\%s::start(%d, "%s", "%s");',
@@ -149,6 +139,16 @@ class AutoFit
      */
     public static function start($requestId, $startDateTime, $endDateTime)
     {
+
+        /**
+         * Remove old agents
+         */
+        if($startDateTime < time() && $endDateTime < time())
+        {
+            static::removeAgent($requestId);
+            return ;
+        }
+
         \Bitrix\Main\Loader::includeModule('iblock');
 
         $arSelect = array(
