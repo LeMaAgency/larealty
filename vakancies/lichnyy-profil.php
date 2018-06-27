aed2775805f280a6190320a7be586b6b7a91d9fd
curlylema <orange_comp@mail.ru>
2018-06-27 15:21:23 +0300

    lichnyy-profil.php and moi-obekty.php corrected

--- vakancies/lichnyy-profil.php
+++ vakancies/lichnyy-profil.php
@@ -1,107 +1,117 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("личный профиль");
-?>	<div class="container">
-        <form name="" class="filter-form form-admin" action="" method="">
-            <div class="filter-form-row">
-			<div class="container-index">
-				<div class="section-title"><span>* Персональные данные</span></div>
-			</div>                
-					<div class="filter-form-column">
-                        <div class="filter-field-title">Имя</div>
+?><div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <ol class="breadcrumb breadcrumb-catalog">
+			<li>
+				<a href="/" title="Главная">
+					Главная
+				</a>
+			</li>
+			<li>
+				<a href="/catalog/" title="Каталог">
+					Каталог
+				</a>
+			</li>
+			<li>
+				<span class="breadcrumb-active">Квартиры</span>
+			</li></ol>            </div>
+        </div>
+    </div>	
+
+<div class="container">
+       	<form name="" class="filter-form form-admin" action="" method="">
+		<div class="container-index">
+				<div class="section-title form-title"><span>* Персональные данные</span></div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+			<div class="filter-field-title">Имя</div>
                         <div class="filter-price">
                             <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Пользователь">
                         </div>
 						<div class="filter-field-title">Отчество</div>
                         <div class="filter-price">
                             <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Ваше отчество">
-                        </div>		
+                        </div>
 						<div class="filter-field-title">Фамилия</div>
                         <div class="filter-price">
                             <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Введите свою фамилию">
-                        </div>		
-                    </div>
-                    <div class="filter-form-column">
-						<div class="filter-field-title">Пол</div>
-						<div class="filter-select">
+                        </div>
+			</div>
+			<div class="col-md-6">
+				<div class="filter-field-title">Пол</div>
+				<div class="filter-select">
                             <a href="#" class="filter-select-link filter-border-color">Выбрать</a>
 								<ul class="filter-select-drop">
                                                                             <li data-value="1">Не определен</li>
                                                                             <li data-value="2">Женский</li>
                                                                             <li data-value="3">Мужской</li>
                                 </ul>
-                        </div>								
+                </div>								
 
-						<div class="filter-field-title">Дата рождения</div>
-                        <div class="filter-price">
+				<div class="filter-field-title">Дата рождения</div>
+                <div class="filter-price">
                                         <input type="date" value="" name="date" id="date" class="filter-price-input filter-max-value-input" placeholder="Дата рождения">
-                        </div>
-						<div class="filter-field-title " >Город</div>
-                        <div class="filter-select">
-                                        <a href="#" class="filter-select-link active filter-border-color">Выбрать</a>
-                        </div>
-                    </div>
-	<div class="container-index">
-        <div class="section-title"><span>* Контактная информация</span></div>
-    </div>
-                        
-						<div class="filter-form-column">
-                            <div class="filter-field-title form-contact-title">№ телефона</div>
-                            <div class="filter-price contact-info">
-                                        <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="№ телефона">
-										<button type="submit" name="" value="" class="filter-submit-btn-admin">Подтвердить</button>
+                </div>
+				<div class="filter-field-title " >Город</div>
+                <div class="filter-select">
+                                        <a href="#" class="filter-select-link filter-border-color">Выбрать</a>
+                </div>
+			</div>
+		</div>
+		<div class="container-index">
+        <div class="section-title  form-title"><span>* Контактная информация</span></div>
+		</div>
+		<div class="row">
+			<div class="col-md-6"><div class="filter-field-title ">№ телефона</div>
+                            <div class="filter-price">
+                                        <input type="text" value="" name="" class="filter-price-input filter-max-value-input btn-accept" placeholder="№ телефона">
+										<button type="submit" name="" value="" class="filter-submit-btn-admin btn-accept">Подтвердить</button>
                             </div>
-                        </div>
-						<div class="filter-form-column">
-                                    <div class="filter-field-title form-contact-title form-contact-email">Email</div>
-                                    <div class="filter-price contact-info">
+			</div>
+			<div class="col-md-6">
+				<div class="filter-field-title  ">Email</div>
+                                    <div class="filter-price">
                                         <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Email">
-										<button type="submit" name="" value="" class="filter-submit-btn-admin">Подтвердить</button>
-                                    </div>
-                                                                                </div>
-
-                                                                              
-
-                                                                     
-                                                </div>
-                        <div class="text-center">
+										<button type="submit" name="" value="" class="filter-submit-btn-admin btn-accept">Подтвердить</button>
+                </div>
+			</div>
+		</div>
+		<div class="text-center margin30">
 						<button type="submit" name="" value="" class="filter-submit-btn margin30 btn-reset">Отменить</button>
 						<button type="submit" name="" value="" class="filter-submit-btn margin30 btn-save">Сохранить</button>
-						</div>
-            <div class="clb"></div>
-        </form>
-		
+		</div>
+		</form>
+	
+	
+		<form name="" class="filter-form form-admin" action="" method="">
 		<div class="container-index">
-        <div class="section-title margin30"><span>* Сменить пароль</span></div>
-    </div>
-	<form name="" class="filter-form form-admin" action="" method="">
-
-            <div class="filter-form-row">
-			
-                                                                                <div class="filter-form-column">
-                                                                                                <div class="filter-field-title">Старый пароль</div>
-                                    <div class="filter-price">
-                                        <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Старый пароль">
-                                    </div>
-                                                                                    </div>
-
-                                                                                <div class="filter-form-column">
-                                                                                                <div class="filter-field-title">Новый пароль</div>
-                                    <div class="filter-price">
-                                        <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Новый пароль">
-                                    </div>
-                                                                                    </div>
-																					<div class="filter-form-column">
-                                                                                                <div class="filter-field-title">Повторите пароль</div>
+        <div class="section-title margin30 form-title"><span>* Сменить пароль</span></div>
+		</div>
+            <div class="row">
+				<div class="col-md-6">
+                    <div class="filter-field-title">Старый пароль</div>
+                    <div class="filter-price">
+                        <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Старый пароль">
+                    </div>
+					<div class="filter-field-title">Повторите пароль</div>
                                     <div class="filter-price">
                                         <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Повторите пароль">
-                                    </div>
-                                                                                    </div>
+                    </div>
+                </div>
 
-                                                                              
-
-                                                                     
-                                                </div>
+                <div class="col-md-6">
+                    <div class="filter-field-title">Новый пароль</div>
+                    <div class="filter-price">
+                        <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Новый пароль">
+                    </div>
+                </div>
+			</div>
                         <button type="submit" name="" value="" class="filter-submit-btn">Сменить пароль</button>
             <div class="clb margin30"></div>
-        </form></div><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+        </form>
+		
+		</div><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- vakancies/moi-obekty.php
+++ vakancies/moi-obekty.php
@@ -1,20 +1,35 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Мои объекты");
-?><div class="container ">
-<div class="container-index">
-        <div class="section-title  margin30"><span>* Мои объекты</span></div>
-	</div>
-		<form name="" class="filter-form form-objects" action="" method="">
+?><div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <ol class="breadcrumb breadcrumb-catalog">
+			<li>
+				<a href="/" title="Главная">
+					Главная
+				</a>
+			</li>
+			<li>
+				<a href="/catalog/" title="Каталог">
+					Каталог
+				</a>
+			</li>
+			<li>
+				<span class="breadcrumb-active">Квартиры</span>
+			</li></ol>            </div>
+        </div>
+    </div>
 
-            <div class="filter-form-row">
-			
-			
-			
-			
-			<div class="filter-form-column objects-column">
-                            <div class="filter-field-title">Тип недвижимости</div>
-                            <div class="filter-select">
+<div class="container ">
+<form name="" class="filter-form form-admin" action="" method="">
+			<div class="container-index">
+			<div class="section-title form-title form-title"><span>* Мои объекты</span></div>
+			</div>
+            <div class="row">
+				<div class="col-md-6">
+                    <div class="filter-field-title">Тип недвижимости</div>
+                    <div class="filter-select">
                                 <a href="#" class="filter-select-link filter-border-color">Выбрать</a>
                                 <ul class="filter-select-drop" >
                                     <li data-value="">Выбрать</li>
@@ -27,10 +42,15 @@ $APPLICATION->SetTitle("Мои объекты");
                                                                             <li data-value="51">Здания</li>
                                                                             <li data-value="152">Дачи</li>
                                                                     </ul>
-                                <input type="hidden" name="arrFilter_pf[REALTY_TYPE]" value="">
-                            </div>
-            </div>
-			<div class="objects-column">
+                                <input type="hidden" name="" value="">
+                    </div>
+					<div class="filter-field-title">Общая площадь, м&sup2;</div>
+                    <div class="filter-price"  >
+                            <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Общая площадь, м&sup2;">
+                    </div>
+					
+				</div>
+			<div class="col-md-6">
                                 <div class="filter-field-title object-number">Кол-во комнат</div>
                                 <div class="filter-num-rooms">
                                     <input name="" type="checkbox" id="n1" value="1" class="filter-input">
@@ -42,53 +62,43 @@ $APPLICATION->SetTitle("Мои объекты");
                                     <input name="" type="checkbox" id="n4" value="4x" class="filter-input">
                                     <label for="n4" class="filter-label">4+</label>
                                 </div>
-            </div>			
-			
-			
-                                                                                <div class="objects-column">
-                                                                                                <div class="filter-field-title">Общая площадь, м&sup2;</div>
-                                    <div class="filter-price"  >
-                                        <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Общая площадь, м&sup2;">
-                                    </div>
-                                                                                    </div>
-
-                                <div class="objects-column">
-                                    <div class="filter-field-title">Этаж</div>
+								
+								<div class="filter-field-title">Этаж</div>
                                     <div class="filter-price">
                                         <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Этаж">
-                                    </div>
                                 </div>
-								<div class="objects-column">
-                                    <div class="filter-field-title">Город</div>
-                                    <div class="filter-price">
+								
+            </div>			
+			
+			<div class="col-md-6">
+                    <div class="filter-field-title">Город</div>
+                    <div class="filter-price">
                                         <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Город">
-                                    </div>
-                                </div>
-								<div class="objects-column">
-                                    <div class="filter-field-title">Улица</div>
-                                    <div class="filter-price">
+                    </div>
+                    
+                    <div class="filter-field-title">Улица</div>
+                    <div class="filter-price">
                                         <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Улица">
-                                    </div>
-                                </div>
-								<div class="objects-column">
-                                    <div class="filter-field-title">№ дома</div>
-                                    <div class="filter-price">
+                    </div>
+            </div>                    
+			<div class="col-md-6">
+                    <div class="filter-field-title">№ дома</div>
+                    <div class="filter-price">
                                         <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="№ дома">
-                                    </div>
-                                </div>
-								<div class="objects-column">
-                                    <div class="filter-field-title">Стоимость недвижимости </div>
-                                    <div class="filter-price">
+                    </div>
+			</div>		
+            <div class="col-md-6 wrap-field-price">                    
+					<div class="filter-field-title field-price" >Стоимость недвижимости </div>
+                    <div class="filter-price">
                                         <input type="text" value="" name="" class="filter-price-input filter-max-value-input" placeholder="Стоимость недвижимости ">
-                                    </div>
-                                </div>
+                    </div>
+            </div>
 																					
                                                                               
 
                                                                      
-                                                </div>
-                        <button type="submit" name="" value="" class="filter-submit-btn">Сохранить объект</button>
+            </div>
+            <button type="submit" name="" value="" class="filter-submit-btn btn-object">Сохранить объект</button>
             <div class="clb margin30"></div>
         </form>
-		
     </div><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
