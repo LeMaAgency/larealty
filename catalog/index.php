67d0a6580a221b41c64989e75b23d3c4a5156d7a
KOPOJI <kopojiq@mail.ru>
2018-07-04 15:04:19 +0300

    hide blocks from catalog

--- catalog/index.php
+++ catalog/index.php
@@ -29,9 +29,6 @@ $rentAndRealtyTypes = array();
 $tmp = \LIblock::getPropEnumValues(LIblock::getPropId('objects', 'RENT_TYPE'));
 foreach($tmp as $code => $data)
     $rentAndRealtyTypes[$code] = $data['VALUE'];
-$tmp = \LIblock::getPropEnumValues(LIblock::getPropId('objects', 'REALTY_TYPE'));
-foreach($tmp as $code => $data)
-    $rentAndRealtyTypes[$code] = $data['VALUE'];
 unset($tmp);
 
 if($inRootDir)
@@ -396,60 +393,116 @@ $GLOBALS['arrFilter']['PROPERTY_RENT_TYPE_VALUE'] = $typeFilter;
 	),
 	false
 );?>
-<?$APPLICATION->IncludeComponent('bitrix:news.list', 'advantages_apartments', array(
-    'DISPLAY_DATE' => 'Y',
-    'DISPLAY_NAME' => 'Y',
-    'DISPLAY_PICTURE' => 'Y',
-    'DISPLAY_PREVIEW_TEXT' => 'Y',
-    'AJAX_MODE' => 'N',
-    'IBLOCK_TYPE' => 'content',
-    'IBLOCK_ID' => '3',
-    'NEWS_COUNT' => '20',
-    'SORT_BY1' => 'ACTIVE_FROM',
-    'SORT_ORDER1' => 'DESC',
-    'SORT_BY2' => 'SORT',
-    'SORT_ORDER2' => 'ASC',
-    'FILTER_NAME' => '',
-    'FIELD_CODE' => array(),
-    'PROPERTY_CODE' => array(),
-    'CHECK_DATES' => 'Y',
-    'DETAIL_URL' => '',
-    'PREVIEW_TRUNCATE_LEN' => '',
-    'ACTIVE_DATE_FORMAT' => 'd.m.Y',
-    'SET_TITLE' => 'N',
-    'SET_BROWSER_TITLE' => 'N',
-    'SET_META_KEYWORDS' => 'N',
-    'SET_META_DESCRIPTION' => 'N',
-    'SET_LAST_MODIFIED' => 'N',
-    'INCLUDE_IBLOCK_INTO_CHAIN' => 'N',
-    'ADD_SECTIONS_CHAIN' => 'N',
-    'HIDE_LINK_WHEN_NO_DETAIL' => 'Y',
-    'PARENT_SECTION' => '',
-    'PARENT_SECTION_CODE' => '',
-    'INCLUDE_SUBSECTIONS' => 'Y',
-    'CACHE_TYPE' => 'A',
-    'CACHE_TIME' => '36000000',
-    'CACHE_FILTER' => 'Y',
-    'CACHE_GROUPS' => 'N',
-    'DISPLAY_TOP_PAGER' => 'Y',
-    'DISPLAY_BOTTOM_PAGER' => 'Y',
-    'PAGER_TITLE' => 'Элементы',
-    'PAGER_SHOW_ALWAYS' => 'N',
-    'PAGER_TEMPLATE' => '',
-    'PAGER_DESC_NUMBERING' => 'N',
-    'PAGER_DESC_NUMBERING_CACHE_TIME' => '36000',
-    'PAGER_SHOW_ALL' => 'N',
-    'PAGER_BASE_LINK_ENABLE' => 'N',
-    'SET_STATUS_404' => 'N',
-    'SHOW_404' => 'N',
-    'MESSAGE_404' => '',
-    'PAGER_BASE_LINK' => '',
-    'PAGER_PARAMS_NAME' => 'arrPager',
-    'AJAX_OPTION_JUMP' => 'N',
-    'AJAX_OPTION_STYLE' => 'Y',
-    'AJAX_OPTION_HISTORY' => 'N',
-    'AJAX_OPTION_ADDITIONAL' => '',
-));?>
+
+<?if(!empty($currentSectionCode) && empty($GLOBALS['arrFilter']['SECTION_CODE'])):?>
+    <? $APPLICATION->IncludeComponent(
+        "bitrix:news.detail",
+        "hypothec",
+        Array(
+            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+            "ADD_ELEMENT_CHAIN" => "N",
+            "ADD_SECTIONS_CHAIN" => "N",
+            "AJAX_MODE" => "N",
+            "AJAX_OPTION_ADDITIONAL" => "",
+            "AJAX_OPTION_HISTORY" => "N",
+            "AJAX_OPTION_JUMP" => "N",
+            "AJAX_OPTION_STYLE" => "Y",
+            "BROWSER_TITLE" => "-",
+            "CACHE_GROUPS" => "Y",
+            "CACHE_TIME" => "36000000",
+            "CACHE_TYPE" => "A",
+            "CHECK_DATES" => "Y",
+            "DETAIL_URL" => "",
+            "DISPLAY_BOTTOM_PAGER" => "N",
+            "DISPLAY_DATE" => "Y",
+            "DISPLAY_NAME" => "Y",
+            "DISPLAY_PICTURE" => "Y",
+            "DISPLAY_PREVIEW_TEXT" => "Y",
+            "DISPLAY_TOP_PAGER" => "N",
+            "ELEMENT_CODE" => "hypothec",
+            "ELEMENT_ID" => "",
+            "FIELD_CODE" => array("", ""),
+            "IBLOCK_ID" => "7",
+            "IBLOCK_TYPE" => "content",
+            "IBLOCK_URL" => "",
+            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+            "MESSAGE_404" => "",
+            "META_DESCRIPTION" => "-",
+            "META_KEYWORDS" => "-",
+            "PAGER_BASE_LINK_ENABLE" => "N",
+            "PAGER_SHOW_ALL" => "N",
+            "PAGER_TEMPLATE" => ".default",
+            "PAGER_TITLE" => "Страница",
+            "PROPERTY_CODE" => array("LIST_ELEMENTS", ""),
+            "SET_BROWSER_TITLE" => "N",
+            "SET_CANONICAL_URL" => "N",
+            "SET_LAST_MODIFIED" => "N",
+            "SET_META_DESCRIPTION" => "N",
+            "SET_META_KEYWORDS" => "N",
+            "SET_STATUS_404" => "N",
+            "SET_TITLE" => "N",
+            "SHOW_404" => "N",
+            "STRICT_SECTION_CHECK" => "N",
+            "USE_PERMISSIONS" => "N",
+            "USE_SHARE" => "N"
+        )
+    ); ?>
+<?else:?>
+    <?$APPLICATION->IncludeComponent('bitrix:news.list', 'advantages_apartments', array(
+        'DISPLAY_DATE' => 'Y',
+        'DISPLAY_NAME' => 'Y',
+        'DISPLAY_PICTURE' => 'Y',
+        'DISPLAY_PREVIEW_TEXT' => 'Y',
+        'AJAX_MODE' => 'N',
+        'IBLOCK_TYPE' => 'content',
+        'IBLOCK_ID' => '3',
+        'NEWS_COUNT' => '20',
+        'SORT_BY1' => 'ACTIVE_FROM',
+        'SORT_ORDER1' => 'DESC',
+        'SORT_BY2' => 'SORT',
+        'SORT_ORDER2' => 'ASC',
+        'FILTER_NAME' => '',
+        'FIELD_CODE' => array(),
+        'PROPERTY_CODE' => array(),
+        'CHECK_DATES' => 'Y',
+        'DETAIL_URL' => '',
+        'PREVIEW_TRUNCATE_LEN' => '',
+        'ACTIVE_DATE_FORMAT' => 'd.m.Y',
+        'SET_TITLE' => 'N',
+        'SET_BROWSER_TITLE' => 'N',
+        'SET_META_KEYWORDS' => 'N',
+        'SET_META_DESCRIPTION' => 'N',
+        'SET_LAST_MODIFIED' => 'N',
+        'INCLUDE_IBLOCK_INTO_CHAIN' => 'N',
+        'ADD_SECTIONS_CHAIN' => 'N',
+        'HIDE_LINK_WHEN_NO_DETAIL' => 'Y',
+        'PARENT_SECTION' => '',
+        'PARENT_SECTION_CODE' => '',
+        'INCLUDE_SUBSECTIONS' => 'Y',
+        'CACHE_TYPE' => 'A',
+        'CACHE_TIME' => '36000000',
+        'CACHE_FILTER' => 'Y',
+        'CACHE_GROUPS' => 'N',
+        'DISPLAY_TOP_PAGER' => 'Y',
+        'DISPLAY_BOTTOM_PAGER' => 'Y',
+        'PAGER_TITLE' => 'Элементы',
+        'PAGER_SHOW_ALWAYS' => 'N',
+        'PAGER_TEMPLATE' => '',
+        'PAGER_DESC_NUMBERING' => 'N',
+        'PAGER_DESC_NUMBERING_CACHE_TIME' => '36000',
+        'PAGER_SHOW_ALL' => 'N',
+        'PAGER_BASE_LINK_ENABLE' => 'N',
+        'SET_STATUS_404' => 'N',
+        'SHOW_404' => 'N',
+        'MESSAGE_404' => '',
+        'PAGER_BASE_LINK' => '',
+        'PAGER_PARAMS_NAME' => 'arrPager',
+        'AJAX_OPTION_JUMP' => 'N',
+        'AJAX_OPTION_STYLE' => 'Y',
+        'AJAX_OPTION_HISTORY' => 'N',
+        'AJAX_OPTION_ADDITIONAL' => '',
+    ));?>
+
 
     <section class="new-flats">
         <div class="container">
@@ -855,6 +908,8 @@ $GLOBALS['arrFilter']['PROPERTY_RENT_TYPE_VALUE'] = $typeFilter;
     </div>
 </section>
 -->
+
+<?endif;?>
 <?
 require_once $_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php';
 ?>
