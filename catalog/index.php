caa5dc824498f6b1b89b6453e47f16a815e0d26c
KOPOJI <kopojiq@mail.ru>
2018-07-11 10:25:07 +0300

    fix catalog

--- catalog/index.php
+++ catalog/index.php
@@ -518,6 +518,9 @@ $GLOBALS['arrFilter']['PROPERTY_RENT_TYPE_VALUE'] = $typeFilter;
                 </div>
             </div>
         </div>
+        <?php
+        $parentSections = array('', '', '');
+        ?>
         <?if(isset($currentSectionCode) && $currentSectionCode === 'kvartiry-komnaty'):?>
             <div class="container">
                 <div class="row">
@@ -548,6 +551,9 @@ $GLOBALS['arrFilter']['PROPERTY_RENT_TYPE_VALUE'] = $typeFilter;
                 </div>
             </div>
         <?else:?>
+            <?php
+                $parentSections = array('doma', 'dachi', 'zemelnyy_uchastok');
+            ?>
             <div class="container">
                 <div class="row">
                     <div class="col-sm-4">
@@ -627,7 +633,7 @@ $GLOBALS['arrFilter']['PROPERTY_RENT_TYPE_VALUE'] = $typeFilter;
                             'ADD_SECTIONS_CHAIN' => 'N',
                             'HIDE_LINK_WHEN_NO_DETAIL' => 'Y',
                             'PARENT_SECTION' => '',
-                            'PARENT_SECTION_CODE' => '',
+                            'PARENT_SECTION_CODE' => $parentSections[0],
                             'INCLUDE_SUBSECTIONS' => 'Y',
                             'CACHE_TYPE' => 'A',
                             'CACHE_TIME' => '36000000',
@@ -707,7 +713,7 @@ $GLOBALS['arrFilter']['PROPERTY_RENT_TYPE_VALUE'] = $typeFilter;
                             'ADD_SECTIONS_CHAIN' => 'N',
                             'HIDE_LINK_WHEN_NO_DETAIL' => 'Y',
                             'PARENT_SECTION' => '',
-                            'PARENT_SECTION_CODE' => '',
+                            'PARENT_SECTION_CODE' => $parentSections[1],
                             'INCLUDE_SUBSECTIONS' => 'Y',
                             'CACHE_TYPE' => 'A',
                             'CACHE_TIME' => '36000000',
@@ -787,7 +793,7 @@ $GLOBALS['arrFilter']['PROPERTY_RENT_TYPE_VALUE'] = $typeFilter;
                             'ADD_SECTIONS_CHAIN' => 'N',
                             'HIDE_LINK_WHEN_NO_DETAIL' => 'Y',
                             'PARENT_SECTION' => '',
-                            'PARENT_SECTION_CODE' => '',
+                            'PARENT_SECTION_CODE' => $parentSections[2],
                             'INCLUDE_SUBSECTIONS' => 'Y',
                             'CACHE_TYPE' => 'A',
                             'CACHE_TIME' => '36000000',
@@ -815,86 +821,88 @@ $GLOBALS['arrFilter']['PROPERTY_RENT_TYPE_VALUE'] = $typeFilter;
                     </div>
                 </div>
             </div>
-            <div role="tabpanel" class="tab-pane" id="four-room">
-                <div class="container">
-                    <div class="row">
-                        <?php
-                        $roomNewElementFilter = array(
-                            '>=PROPERTY_ROOMS_COUNT' => 4,
-                            'PROPERTY_RENT_TYPE_VALUE' => $typeFilter,
-                            'SECTION_CODE' => $sectionCodes,
-                        );
-                        ?>
-                        <?$APPLICATION->IncludeComponent('bitrix:news.list', 'rooms', array(
-                            'DISPLAY_DATE' => 'Y',
-                            'DISPLAY_NAME' => 'Y',
-                            'DISPLAY_PICTURE' => 'Y',
-                            'DISPLAY_PREVIEW_TEXT' => 'Y',
-                            'AJAX_MODE' => 'N',
-                            'IBLOCK_TYPE' => 'realty',
-                            'IBLOCK_ID' => '2',
-                            'NEWS_COUNT' => '3',
-                            'SORT_BY1' => 'ACTIVE_FROM',
-                            'SORT_ORDER1' => 'DESC',
-                            'SORT_BY2' => 'SORT',
-                            'SORT_ORDER2' => 'ASC',
-                            'FILTER_NAME' => 'roomNewElementFilter',
-                            'FIELD_CODE' => array(),
-                            'PROPERTY_CODE' => array(
-                                0 => "ROOMS_COUNT",
-                                1 => "PRICE",
-                                2 => "ADDRESS",
-                                3 => "YEAR",
-                                4 => "MAP",
-                                5 => "PLACEMENT",
-                                6 => "LAYOUT",
-                                7 => "SQUARE",
-                                8 => "SIDE",
-                                9 => "REALTY_TYPE",
-                                10 => "STAGE",
-                                11 => "STAGES_COUNT",
-                            ),
-                            'CHECK_DATES' => 'Y',
-                            'DETAIL_URL' => '',
-                            'PREVIEW_TRUNCATE_LEN' => '',
-                            'ACTIVE_DATE_FORMAT' => 'd.m.Y',
-                            'SET_TITLE' => 'N',
-                            'SET_BROWSER_TITLE' => 'N',
-                            'SET_META_KEYWORDS' => 'N',
-                            'SET_META_DESCRIPTION' => 'N',
-                            'SET_LAST_MODIFIED' => 'N',
-                            'INCLUDE_IBLOCK_INTO_CHAIN' => 'N',
-                            'ADD_SECTIONS_CHAIN' => 'N',
-                            'HIDE_LINK_WHEN_NO_DETAIL' => 'Y',
-                            'PARENT_SECTION' => '',
-                            'PARENT_SECTION_CODE' => '',
-                            'INCLUDE_SUBSECTIONS' => 'Y',
-                            'CACHE_TYPE' => 'A',
-                            'CACHE_TIME' => '36000000',
-                            'CACHE_FILTER' => 'Y',
-                            'CACHE_GROUPS' => 'N',
-                            'DISPLAY_TOP_PAGER' => 'Y',
-                            'DISPLAY_BOTTOM_PAGER' => 'Y',
-                            'PAGER_TITLE' => 'Элементы',
-                            'PAGER_SHOW_ALWAYS' => 'N',
-                            'PAGER_TEMPLATE' => '',
-                            'PAGER_DESC_NUMBERING' => 'N',
-                            'PAGER_DESC_NUMBERING_CACHE_TIME' => '36000',
-                            'PAGER_SHOW_ALL' => 'N',
-                            'PAGER_BASE_LINK_ENABLE' => 'N',
-                            'SET_STATUS_404' => 'N',
-                            'SHOW_404' => 'N',
-                            'MESSAGE_404' => '',
-                            'PAGER_BASE_LINK' => '',
-                            'PAGER_PARAMS_NAME' => 'arrPager',
-                            'AJAX_OPTION_JUMP' => 'N',
-                            'AJAX_OPTION_STYLE' => 'Y',
-                            'AJAX_OPTION_HISTORY' => 'N',
-                            'AJAX_OPTION_ADDITIONAL' => '',
-                        ));?>
+            <?if(empty($parentSections[0])):?>
+                <div role="tabpanel" class="tab-pane" id="four-room">
+                    <div class="container">
+                        <div class="row">
+                            <?php
+                            $roomNewElementFilter = array(
+                                '>=PROPERTY_ROOMS_COUNT' => 4,
+                                'PROPERTY_RENT_TYPE_VALUE' => $typeFilter,
+                                'SECTION_CODE' => $sectionCodes,
+                            );
+                            ?>
+                            <?$APPLICATION->IncludeComponent('bitrix:news.list', 'rooms', array(
+                                'DISPLAY_DATE' => 'Y',
+                                'DISPLAY_NAME' => 'Y',
+                                'DISPLAY_PICTURE' => 'Y',
+                                'DISPLAY_PREVIEW_TEXT' => 'Y',
+                                'AJAX_MODE' => 'N',
+                                'IBLOCK_TYPE' => 'realty',
+                                'IBLOCK_ID' => '2',
+                                'NEWS_COUNT' => '3',
+                                'SORT_BY1' => 'ACTIVE_FROM',
+                                'SORT_ORDER1' => 'DESC',
+                                'SORT_BY2' => 'SORT',
+                                'SORT_ORDER2' => 'ASC',
+                                'FILTER_NAME' => 'roomNewElementFilter',
+                                'FIELD_CODE' => array(),
+                                'PROPERTY_CODE' => array(
+                                    0 => "ROOMS_COUNT",
+                                    1 => "PRICE",
+                                    2 => "ADDRESS",
+                                    3 => "YEAR",
+                                    4 => "MAP",
+                                    5 => "PLACEMENT",
+                                    6 => "LAYOUT",
+                                    7 => "SQUARE",
+                                    8 => "SIDE",
+                                    9 => "REALTY_TYPE",
+                                    10 => "STAGE",
+                                    11 => "STAGES_COUNT",
+                                ),
+                                'CHECK_DATES' => 'Y',
+                                'DETAIL_URL' => '',
+                                'PREVIEW_TRUNCATE_LEN' => '',
+                                'ACTIVE_DATE_FORMAT' => 'd.m.Y',
+                                'SET_TITLE' => 'N',
+                                'SET_BROWSER_TITLE' => 'N',
+                                'SET_META_KEYWORDS' => 'N',
+                                'SET_META_DESCRIPTION' => 'N',
+                                'SET_LAST_MODIFIED' => 'N',
+                                'INCLUDE_IBLOCK_INTO_CHAIN' => 'N',
+                                'ADD_SECTIONS_CHAIN' => 'N',
+                                'HIDE_LINK_WHEN_NO_DETAIL' => 'Y',
+                                'PARENT_SECTION' => '',
+                                'PARENT_SECTION_CODE' => '',
+                                'INCLUDE_SUBSECTIONS' => 'Y',
+                                'CACHE_TYPE' => 'A',
+                                'CACHE_TIME' => '36000000',
+                                'CACHE_FILTER' => 'Y',
+                                'CACHE_GROUPS' => 'N',
+                                'DISPLAY_TOP_PAGER' => 'Y',
+                                'DISPLAY_BOTTOM_PAGER' => 'Y',
+                                'PAGER_TITLE' => 'Элементы',
+                                'PAGER_SHOW_ALWAYS' => 'N',
+                                'PAGER_TEMPLATE' => '',
+                                'PAGER_DESC_NUMBERING' => 'N',
+                                'PAGER_DESC_NUMBERING_CACHE_TIME' => '36000',
+                                'PAGER_SHOW_ALL' => 'N',
+                                'PAGER_BASE_LINK_ENABLE' => 'N',
+                                'SET_STATUS_404' => 'N',
+                                'SHOW_404' => 'N',
+                                'MESSAGE_404' => '',
+                                'PAGER_BASE_LINK' => '',
+                                'PAGER_PARAMS_NAME' => 'arrPager',
+                                'AJAX_OPTION_JUMP' => 'N',
+                                'AJAX_OPTION_STYLE' => 'Y',
+                                'AJAX_OPTION_HISTORY' => 'N',
+                                'AJAX_OPTION_ADDITIONAL' => '',
+                            ));?>
+                        </div>
                     </div>
                 </div>
-            </div>
+            <?endif;?>
         </div>
     </section>
     <!--
