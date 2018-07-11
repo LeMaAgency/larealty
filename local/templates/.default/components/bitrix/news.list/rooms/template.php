7ddffe4f42ea493ed06ca1e38ac91771199752e5
KOPOJI <kopojiq@mail.ru>
2018-07-11 10:38:29 +0300

    fix catalog

--- local/templates/.default/components/bitrix/news.list/rooms/template.php
+++ local/templates/.default/components/bitrix/news.list/rooms/template.php
@@ -56,10 +56,10 @@ $data = new TH($this);
                                         <div class="item-info__inner__img item-info__inner__img_floor"></div>
                                         <div class="item-info__inner__content">
                                             <?if($item->get('IS_HOUSE_OR_LOT')):?>
-                                                <?if($item->propFilled('STAGE_COUNT')):?>
+                                                <?if($item->propFilled('STAGES_COUNT')):?>
                                                     <div class="item-info-name">Этажность</div>
                                                     <div class="item-info-value">
-                                                        <?=$item->propVal('STAGE_COUNT');?>
+                                                        <?=$item->propVal('STAGES_COUNT');?>
                                                     </div>
                                                 <?endif;?>
                                             <?else:?>
