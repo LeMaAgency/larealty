adf28275f97c42b2edd3d7765a3b56268812c424
DoNotEvenThink <alekseivasin1993@gmail.com>
2018-11-23 17:19:29 +0300

    fix

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -202,22 +202,24 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
                         <? $APPLICATION->IncludeFile(SITE_DIR . 'include/footer/subscribe-text.php'); ?>
                     </h3>
                     <? $APPLICATION->IncludeComponent(
-                        "bitrix:subscribe.edit",
-                        "",
-                        Array(
-                            "AJAX_MODE" => "N",
-                            "AJAX_OPTION_ADDITIONAL" => "",
-                            "AJAX_OPTION_HISTORY" => "N",
-                            "AJAX_OPTION_JUMP" => "N",
-                            "AJAX_OPTION_STYLE" => "Y",
-                            "ALLOW_ANONYMOUS" => "Y",
-                            "CACHE_TIME" => "3600",
-                            "CACHE_TYPE" => "A",
-                            "SET_TITLE" => "N",
-                            "SHOW_AUTH_LINKS" => "N",
-                            "SHOW_HIDDEN" => "N"
-                        )
-                    ); ?>
+	"bitrix:subscribe.edit", 
+	".default", 
+	array(
+		"AJAX_MODE" => "Y",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"ALLOW_ANONYMOUS" => "Y",
+		"CACHE_TIME" => "3600",
+		"CACHE_TYPE" => "A",
+		"SET_TITLE" => "N",
+		"SHOW_AUTH_LINKS" => "N",
+		"SHOW_HIDDEN" => "N",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+); ?>
                     <div class="form-txt">
                         <? $APPLICATION->IncludeFile(SITE_DIR . 'include/footer/subscribe-subtext.php'); ?>
                     </div>
