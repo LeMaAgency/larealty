6c2fb822d5f6b863f1c47165e79010aed3a46cad
SultanMashukovH <mashukov.s@lema.agency>
2018-11-28 16:15:29 +0300

    Для блока "Новые предложения" на главной странице убрано ограничение в 6 элементов

--- local/templates/.default/components/bitrix/news.list/main_new_objects/template.php
+++ local/templates/.default/components/bitrix/news.list/main_new_objects/template.php
@@ -11,14 +11,10 @@
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
-$maxShowCount = 0;
 ?>
 <div class="newoffer-slider">
     <? foreach ($arResult["ITEMS"] as $arItem): ?>
         <?
-        if($maxShowCount === 6)
-            break;
-        $maxShowCount++;
         $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
         $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 
