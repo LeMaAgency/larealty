b933bcab120a74b7b8677547f05343bd8568eb8b
DoNotEvenThink <alekseivasin1993@gmail.com>
2018-12-19 14:03:06 +0300

    fix

--- index.php
+++ index.php
@@ -28,7 +28,81 @@ $APPLICATION->SetTitle("Агентство недвижимости Lexhome");
 <section class="realties">
     <div class="container">
         <h2 class="section-h2">Все виды недвижимости</h2>
-        <? $APPLICATION->IncludeComponent(
+        <div class="row">
+            <div class="col-md-8">
+                <div class="realties-item realties-first">
+                    <div class="realties-overlay"></div>
+                    <div class="realties-title">Жилая в городе</div>
+                    <div class="realties-list">
+                        <!--<ul>
+                            <li><a href="#">Все жилые комплексы <span>163</span></a></li>
+                            <li><a href="#">Дома <span>9</span></a></li>
+                            <li><a href="#">Квартиры <span>3 149</span></a></li>
+                        </ul>
+                        <ul>
+                            <li><a href="#">Пентхаусы <span>166</span></a></li>
+                            <li><a href="#">Таунхаусы <span>27</span></a></li>
+                            <li><a href="#">Новостройки <span>52</span></a></li>
+                        </ul>
+                        <ul>
+                            <li><a href="#">Аренда <span>275</span></a></li>
+                            <li><a href="#">Дисконты <span>27</span></a></li>
+                        </ul>-->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="realties-item realties-second">
+                    <div class="realties-overlay"></div>
+                    <div class="realties-title">Загородная</div>
+                    <div class="realties-list">
+                        <!--<ul>
+                            <li><a href="#">Дом <span>1 184</span></a></li>
+                            <li><a href="#">Квартира <span>174</span></a></li>
+                            <li><a href="#">Таунхаус <span>251</span></a></li>
+                        </ul>
+                        <ul>
+                            <li><a href="#">Поселки <span>85</span></a></li>
+                            <li><a href="#">Участок <span>526</span></a></li>
+                            <li><a href="#">Аренда <span>277</span></a></li>
+                        </ul>-->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="realties-item realties-third">
+                    <div class="realties-overlay"></div>
+                    <div class="realties-title">Коммерческая</div>
+                    <div class="realties-list">
+                        <!--<ul>
+                            <li><a href="#">Аренда <span>764</span></a></li>
+                            <li><a href="#">Продажа <span>100</span></a></li>
+                            <li><a href="#">Арендный бизнес <span>33</span></a></li>
+                        </ul>-->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="realties-item realties-fourth">
+                    <div class="realties-overlay"></div>
+                    <div class="realties-title">Зарубежная</div>
+                    <div class="realties-list">
+                        <!--<ul>
+                            <li><a href="#">Вилла <span>214</span></a></li>
+                            <li><a href="#">Апартаменты <span>124</span></a></li>
+                            <li><a href="#">Пентхаус <span>27</span></a></li>
+                        </ul>
+                        <ul>
+                            <li><a href="#">Таунхаус <span>18</span></a></li>
+                        </ul>-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?/* $APPLICATION->IncludeComponent(
             "bitrix:catalog.section.list",
             "property_types",
             Array(
@@ -71,7 +145,7 @@ $APPLICATION->SetTitle("Агентство недвижимости Lexhome");
             ),
             false
         );
-        ?>
+        */?>
         <div class="realties-consultation">
             <div class="consultation-text">
 
