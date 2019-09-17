1f18cce379f0b3fccb49872a71f0d075eb27ba51
SultanMashukovH <mashukov.s@lema.agency>
2019-09-17 16:53:27 +0300

    In footer added social links

--- include/footer/social-link.php
+++ include/footer/social-link.php
@@ -1,4 +1,4 @@
-<a class="social-vk" href="#"></a>
-<a class="social-in" href="#"></a>
-<a class="social-fb" href="#"></a>
-<a class="social-tw" href="#"></a>
+<a class="social-vk" href="https://vk.com/lexhome" target="_blank"></a>
+<a class="social-in" href="https://www.instagram.com/lexhome.su/" target="_blank"></a>
+<a class="social-fb" href="https://www.facebook.com/LEXHome.su/" target="_blank"></a>
+<!--<a class="social-tw" href="#"></a>-->
