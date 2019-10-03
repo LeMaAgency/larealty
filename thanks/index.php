71e0586065e96c49e0df64959170bf8eba08d389
SultanMashukovH <mashukov.s@lema.agency>
2019-10-03 16:53:27 +0300

    Added thanks page for forms

---
+++ thanks/index.php
@@ -0,0 +1,14 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Спасибо за заявку!");
+?>
+<div class="container">
+    <div class="thanks_page">
+        <h1 class="thanks_title">Cпасибо за заявку!</h1>
+        <div class="thanks_text">
+            Спасибо, что оставили заявку на нашем сайте. В скором времени, менеджер свяжется с Вами и уточнит все детали!
+        </div>
+    </div>
+</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
